a362ecfdb0260fd6d8f168aac4e603232065f967
MariosLevi <76508477+MariosLevi@users.noreply.github.com>
2020-12-25 01:54:38 -0800

    Add files via upload

---
+++ GoogleMaps_Elevation_API.py
@@ -0,0 +1,19 @@
+from pprint import pprint #pip install pprint
+import googlemaps #pip install googlemaps
+import math
+import ast
+
+API_KEY = "AIzaSyAybh8hXbXTIvOaHglmZPbyvJrYJ1ujFUc"
+
+map_client = googlemaps.Client(API_KEY)
+
+#location of the spot you want the elevation to
+location = 28.4723325,25.0608467
+
+response = map_client.elevation(location) #Creates an elevation list
+response_no_brackets = ast.literal_eval((str(response)[1:-1])) #converting the list into a dictionary
+
+pure_elevation = response_no_brackets["elevation"] #outputs a float like 139.9674987792969
+output_elevation = str(math.ceil(pure_elevation*100)/100) #Ceiling function rounds up to 2 decimal points -> 139.97
+
+print("The elevation of provided coordinates '" + str(location) + "' is " + output_elevation + " meters.")
