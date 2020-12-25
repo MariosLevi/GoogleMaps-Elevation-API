89a91cba5dbf66a8dc9f3dee79c5ca9af5cd5481
MariosLevi <76508477+MariosLevi@users.noreply.github.com>
2020-12-25 01:56:02 -0800

    Update GoogleMaps_Elevation_API.py

--- GoogleMaps_Elevation_API.py
+++ GoogleMaps_Elevation_API.py
@@ -1,7 +1,7 @@
 from pprint import pprint #pip install pprint
 import googlemaps #pip install googlemaps
+import ast #pip install ast
 import math
-import ast
 
 API_KEY = "YOUR KEY"
 
@@ -10,8 +10,11 @@ map_client = googlemaps.Client(API_KEY)
 #location of the spot you want the elevation to
 location = 28.4723325,25.0608467
 
-response = map_client.elevation(location) #Creates an elevation list
-response_no_brackets = ast.literal_eval((str(response)[1:-1])) #converting the list into a dictionary
+#Creates an elevation list
+response = map_client.elevation(location)
+
+#Converting the list into a dictionary
+response_no_brackets = ast.literal_eval((str(response)[1:-1])) 
 
 pure_elevation = response_no_brackets["elevation"] #outputs a float like 139.9674987792969
 output_elevation = str(math.ceil(pure_elevation*100)/100) #Ceiling function rounds up to 2 decimal points -> 139.97
